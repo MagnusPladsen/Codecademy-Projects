62384dbb8f993a0bdf975f5c7f6b38dd10c7597e
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2022-06-17 11:46:17 +0200

    Finished exercise 11
    - Finished exercise 11 of 14

--- react-router-lesson-starter/src/components/Profile.js
+++ react-router-lesson-starter/src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Route, Link, useRouteMatch } from "react-router-dom";
+import { Route, Link, useRouteMatch, Redirect } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 import EditProfileForm from "./EditProfileForm";
 
@@ -11,6 +11,9 @@ export default function Profile () {
   // call useRouteMatch() to get the url and path
   const { url, path } = useRouteMatch();
   // use loggedIn to return a Redirect
+  if (!loggedIn) {
+    return <Redirect to="/sign-up" />
+  }
   
   return (
     <main>
